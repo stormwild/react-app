689e5eaae6433f3de4217a888a3f9af6734fb479
Alexander R. Torrijos <artorrijos@gmail.com>
2022-01-22 16:52:52 +0800

    React working

--- index.js
+++ index.js
@@ -6,10 +6,11 @@
 
 // app.appendChild(el);
 
-const el = React.createElement(
-  "div",
-  { className: "orange" },
-  "Hello From Pluralsight and React"
-);
-
-ReactDOM.render(el, document.getElementById("app"));
+const Hello = function(props) {
+    return React.createElement(
+        "h1",
+        { className: "orange" },
+        "Hello From Pluralsight and React: " + props.time
+      );      
+}
+ReactDOM.render(React.createElement(Hello, { time: new Date().toLocaleDateString() }, null), document.getElementById("app"));
