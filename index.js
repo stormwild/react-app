cce51cfb1aa4879d01ce089e542fad019fb4976f
Alexander R. Torrijos <artorrijos@gmail.com>
2022-01-22 17:01:21 +0800

    State based timer working

--- index.js
+++ index.js
@@ -7,11 +7,32 @@
 // app.appendChild(el);
 
 class Hello extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: new Date().toLocaleString(),
+    };
+  }
+
+  tick() {
+      this.setState(() => {
+          return ({ time: new Date().toLocaleString()})
+      })
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+      clearInterval(this.interval)
+  }
+
   render() {
     return React.createElement(
       "h1",
       { className: "orange" },
-      "Hello From Pluralsight and React: " + this.props.time
+      "Hello From Pluralsight and React: " + this.state.time
     );
   }
 }
