781f998c8bf4ccb7ff07a4168eb0287be37fd17a
Alexander R. Torrijos <artorrijos@gmail.com>
2022-01-22 16:46:52 +0800

    React sample working

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const app = document.getElementById('app');
+// const app = document.getElementById('app');
 
-const el = document.createElement('div');
+// const el = document.createElement('div');
+// el.className = 'orange';
+// el.innerText = 'Hello'
 
-el.className = 'orange';
-el.innerText = 'Hello'
+// app.appendChild(el);
 
-app.appendChild(el);
+const el = React.createElement(
+  "div",
+  { className: "orange" },
+  "Hello From Pluralsight and React"
+);
+
+ReactDOM.render(el, document.getElementById("app"));
